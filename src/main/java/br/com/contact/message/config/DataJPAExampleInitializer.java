6545254bf505eb1b7dab7e2eec7fb6d5905734dc
alexandregama <alexandre.gama.lima@gmail.com>
2016-02-29 08:40:54 -0300

    Implements a WebApplicationInitializer to enable configuration without web.xml

---
+++ src/main/java/br/com/contact/message/config/DataJPAExampleInitializer.java
@@ -0,0 +1,29 @@
+package br.com.contact.message.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class DataJPAExampleInitializer implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		// Loading application context
+		AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+		rootContext.register(ApplicationContext.class);
+
+		// Dispatcher servlet
+		ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", new DispatcherServlet(rootContext));
+		dispatcher.setLoadOnStartup(1);
+		dispatcher.addMapping("/");
+
+		// Context loader listener
+		servletContext.addListener(new ContextLoaderListener(rootContext));
+	}
+
+}
